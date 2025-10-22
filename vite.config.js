fd7d0e7226d732f93c5832c5924feba1421f5035
esencialwork <esencialworkus@gmail.com>
2025-10-22 10:47:36 -0700

    fix: add alias for /src to Vite config
    Added resolve.alias mapping "/src" to the src directory in vite.config.js to resolve build error during deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 
 export default defineConfig({
   plugins: [react()],
+      resolve: {
+      alias: {
+        '/src': resolve(__dirname, 'src')
+      }
+    },
+  
   server: {
     port: 5173
   }
